30bae290d1777d33c1ddc9ab5341bdbbf5ab7ad3
AlexBirdSong <alexwhitechoco@gmail.com>
2020-11-12 23:17:36 +0300

    add selector lesson result learning jQuery

---
+++ lesson 2.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('div').addClass('selected');// выбор по тегу
+    $('#idelem').addClass('selected');// выбор по id
+    $('.classelem').addClass('selected');// выбор по классу
+});
