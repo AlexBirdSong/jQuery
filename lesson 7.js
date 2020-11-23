56ba003a3682c1a13c3141acd49070246379cb53
AlexBirdSong <alexwhitechoco@gmail.com>
2020-11-23 22:18:45 +0300

    intermediate lesson solution

--- lesson 7.js
+++ lesson 7.js
@@ -1,4 +1,30 @@
 $(document).ready(function(){
 
+    $('#testBox').text('Новый текст');
    
+    // $('#testBox').html('Новый <b>текст</b>');
+
+    // $newcontent = $(
+    //     '<h4>Заголовок</h4>' +
+    //     '<p>Абзац с тестом</p>' +
+    //     '<ul>' +
+    //     '<li>Пункт 1</li>' +
+    //     '<li>Пункт 2</li>' +
+    //     '<li>Пункт 3</li>' +
+    //     '</ul>' 
+    // );
+
+    // $newcontent.hide();
+       
+    // $('#testBox').html($newcontent);
+
+    // $newcontent.fadeIn();
+
+    $('#testBox').prepend('Текст в начало ');
+
+    $('#testBox').append('Текст в конец ');
+
+    $('#testBox').before('Текст в начало ');
+
+    $('#testBox').after('Текст в конец ');
 });
